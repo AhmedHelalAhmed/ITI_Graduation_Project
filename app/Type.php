03c0d7a9e768c54a420d03769fef116f0fe3a764
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-29 15:48:22 +0200

    add relationship to type model

--- app/Type.php
+++ app/Type.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    //
+    protected $fillable = [
+        'description',
+    ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
