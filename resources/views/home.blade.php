1432bf9f0d6b5c871c3b0c6f0265aa874216d986
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-29 12:02:04 +0200

    modify the view data to fit my project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header text-center">Now publishes</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="card-body">
+                        <a href="#">info 1</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
