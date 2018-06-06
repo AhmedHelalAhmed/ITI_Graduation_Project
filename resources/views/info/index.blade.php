4c68f946108a45d44031e4de248bf28e0fdbc766
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-06-06 08:04:56 +0200

    add basic share to social media

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -12,7 +12,45 @@
                         <strong class="d-block text-gray-dark">@ {{  $info_element->user->name }}</strong>
                         <a href="/info/{{ $info_element->id }}">{{ $info_element->title }}</a>
                     </p>
+
+                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ url('/').'/info/'. $info_element->id }}"
+                       target="_blank">
+                        Share on Facebook
+                    </a>
+                    <a href="https://twitter.com/intent/tweet?url={{ url('/').'/info/'. $info_element->id  }}"
+                       target="_blank">
+                        Share on Twitter
+                    </a>
+                    <a href="https://plus.google.com/share?url={{ url('/').'/info/'. $info_element->id  }}"
+                       target="_blank">
+                        Share on Google
+                    </a>
+
                 </div>
             @endforeach
         </div>
+        <script>
+
+            var popupMeta = {
+                width: 400,
+                height: 400
+            }
+            $(document).on('click', '.social-share', function (event) {
+                event.preventDefault();
+
+                var vPosition = Math.floor(($(window).width() - popupMeta.width) / 2),
+                    hPosition = Math.floor(($(window).height() - popupMeta.height) / 2);
+
+                var url = $(this).attr('href');
+                var popup = window.open(url, 'Social Share',
+                    'width=' + popupMeta.width + ',height=' + popupMeta.height +
+                    ',left=' + vpPsition + ',top=' + hPosition +
+                    ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
+
+                if (popup) {
+                    popup.focus();
+                    return false;
+                }
+            });
+        </script>
 @endsection
