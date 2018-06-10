78c4a2271763142236f3a77bb156928d2061f905
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-06-10 15:50:37 +0200

    organize code into files and include it with jquery before

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,19 +1,29 @@
-var postId = 0;
-$('.vote').on('click', function(event) {
-    event.preventDefault();
-    articleId = event.target.parentNode.id;
-    var isvote = event.target.previousElementSibling == null;
-    $.ajax({
-        method: 'POST',
-        url: urlvote,
-        data: {isvote: isvote, articleId: articleId, _token: token}
-    })
-        .done(function() {
-            event.target.innerText = isvote ? event.target.innerText == 'Up' ? 'You up this article' : 'Up' : event.target.innerText == 'Down' ? 'You down this article' : 'Down';
-            if (isvote) {
-                event.target.nextElementSibling.innerText = 'Down';
-            } else {
-                event.target.previousElementSibling.innerText = 'Up';
-            }
-        });
+$(function () {
+
+
+    // Start Rate
+    var postId = 0;
+    $('.vote').on('click', function (event) {
+        event.preventDefault();
+        articleId = event.target.parentNode.id;
+        var isvote = event.target.previousElementSibling == null;
+        $.ajax({
+            method: 'POST',
+            url: urlvote,
+            data: {isvote: isvote, articleId: articleId, _token: token}
+        })
+            .done(function () {
+                event.target.innerText = isvote ? event.target.innerText == 'Up' ? 'You up this article' : 'Up' : event.target.innerText == 'Down' ? 'You down this article' : 'Down';
+                if (isvote) {
+                    event.target.nextElementSibling.innerText = 'Down';
+                } else {
+                    event.target.previousElementSibling.innerText = 'Up';
+                }
+            });
+    });
+    // End Rate
+
+
 });
+
+
--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -37,15 +37,14 @@
                                 <a href="/info/{{ $info_element->id }}">Read more</a>
 
                                 <div class="d-flex justify-content-between align-items-center">
+
                                     <div class="btn-group" id="{{ $info_element->id }}">
 
                                         <button type="button"
-                                                class="btn btn-sm btn-outline-secondary vote">{{ Auth::user()->votes()->where('article_id', $info_element->id)->first() ? Auth::user()->votes()->where('article_id', $info_element->id)->first()->vote == 1 ? 'You Vote up to this article' : 'Up' : 'Up'  }}
-                                        </button>
+                                                class="btn btn-sm btn-outline-warning vote">{{ Auth::user()->votes()->where('article_id', $info_element->id)->first() ? Auth::user()->votes()->where('article_id', $info_element->id)->first()->vote == 1 ? 'You up this article' : 'Up' : 'Up'  }}</button>
 
                                         <button type="button"
-                                                class="btn btn-sm btn-outline-secondary vote">{{ Auth::user()->votes()->where('article_id', $info_element->id)->first() ? Auth::user()->votes()->where('article_id', $info_element->id)->first()->vote == 0 ? 'You vote Down to this article' : 'Down' : 'Down'  }}
-                                        </button>
+                                                class="btn btn-sm btn-outline-danger vote">{{ Auth::user()->votes()->where('article_id', $info_element->id)->first() ? Auth::user()->votes()->where('article_id', $info_element->id)->first()->vote == 0 ? 'You Down this article' : 'Down' : 'Down'  }}</button>
                                     </div>
 
                                     {{--<div class="social_media">--}}
@@ -53,14 +52,11 @@
                                     {{--</div>--}}
 
 
-
                                 </div>
 
                                 <small class="text-muted">
                                     <time datatime="{{  $info_element->created_at }}">
                                         {{ Carbon\Carbon::parse($info_element->created_a)->toDayDateTimeString() }}
-
-
                                     </time>
                                 </small>
                             </div>
@@ -85,43 +81,30 @@
     </div>
 
 
-    <script>
-
-        var popupMeta = {
-            width: 400,
-            height: 400
-        }
-
-        $(document).on('click', '.social-share', function (event) {
-            event.preventDefault();
-
-            var vPosition = Math.floor(($(window).width() - popupMeta.width) / 2),
-                hPosition = Math.floor(($(window).height() - popupMeta.height) / 2);
-
-            var url = $(this).attr('href');
-            var popup = window.open(url, 'Social Share',
-                'width=' + popupMeta.width + ',height=' + popupMeta.height +
-                ',left=' + vpPsition + ',top=' + hPosition +
-                ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
-
-            if (popup) {
-                popup.focus();
-                return false;
-            }
-        });
-    </script>
-
-    <!-- rate -->
-    <script>
-        var token = '{{ Session::token() }}';
-        var urlvote = '{{ route('vote') }}';
-    </script>
-    <script src="{{ asset('/js/vote.js') }}"></script>
-
 @endsection
 
 
 
 @section('sidebar')
     @include('layouts.sidebar')
+@endsection
+
+
+@section('include_files_body')
+    <!-- Start Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- End Jquery -->
+
+    <!-- Start Share -->
+    <script src="{{ asset('/js/social-share.js') }}"></script>
+    <!-- End Share -->
+
+    <!-- Start Rate -->
+    <script>
+        var token = '{{ Session::token() }}';
+        var urlvote = '{{ route('vote') }}';
+    </script>
+    <script src="{{ asset('/js/vote.js') }}"></script>
+    <!-- End Rate -->
+
 @endsection
